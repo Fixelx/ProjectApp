7d33cc4b7c8ccd7e830ebd13d7be479c70a2bc53
Fixelx <69038808+Fixelx@users.noreply.github.com>
2026-07-01 00:25:13 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
 
 INSTALLED_APPS = [
     'accounts',
