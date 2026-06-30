d26c4ce5c7368631fff86b0754a3990045688339
Fixelx <69038808+Fixelx@users.noreply.github.com>
2026-07-01 00:05:31 +0200

    Update apps.py

--- invoices/apps.py
+++ invoices/apps.py
@@ -1,9 +1,19 @@
 from django.apps import AppConfig
 
+
 class InvoicesConfig(AppConfig):
     name = "invoices"
 
     def ready(self):
+        import sys
+
+        if any(cmd in sys.argv for cmd in ("migrate", "makemigrations", "shell", "test")):
+            return
+
+        from django.db import connection
+        if "django_apscheduler_djangojob" not in connection.introspection.table_names():
+            return
+
         from django_apscheduler.jobstores import DjangoJobStore
         from apscheduler.schedulers.background import BackgroundScheduler
         from . import scheduler
